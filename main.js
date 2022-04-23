99672e9b7f9eb8150ce309487810dd8920b1211f
YanPetrov7 <yanemerald2004@gmail.com>
2022-04-23 11:52:49 +0300

    create func rand

--- main.js
+++ main.js
@@ -6,8 +6,21 @@ const readLine = require('readline-sync');
 const readFile = fs.readFileSync('text.txt', 'utf8');
 const arrText = readFile.split('\n');
 
+const rand = arr => {
+    const length = arr.length;
+    let randNumber = Math.floor(Math.random() * length);
+        if(arr == prossAndCons){
+            let randNumbers = [];
+            for(let i = 0; i < 4; i++){
+                randNumbers[i] = Math.floor(Math.random() * length);
+            }
+            return randNumbers;
+        }
+    return randNumber;
+}
+
 class Person {
     constructor( gender, age, childfree, job, health, stageDisease ,phobia, hobby, firstFact, secondFact, backpack, actCard ){
-        
+
     }
 }
