33ec342e2d856535f55abd72843e08047d0c9517
YanPetrov7 <yanemerald2004@gmail.com>
2022-04-23 12:06:22 +0300

    fill Persons constructor

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const rand = arr => {
     let randNumber = Math.floor(Math.random() * length);
         if(arr == prossAndCons){
             let randNumbers = [];
-            for(let i = 0; i < 4; i++){
+            for(let i = 0; i < arr.length; i++){
                 randNumbers[i] = Math.floor(Math.random() * length);
             }
             return randNumbers;
@@ -21,6 +21,17 @@ const rand = arr => {
 
 class Person {
     constructor( gender, age, childfree, job, health, stageDisease ,phobia, hobby, firstFact, secondFact, backpack, actCard ){
-
+        this.gender = gender[rand(gender)];
+        this.age = age[rand(age)];
+        this.childfree = childfree[rand(childfree)];
+        this.job = job[rand(job)];
+        this.health = health[rand(health)];
+        this.stageDisease = stageDisease[rand(stageDisease)] + '%';
+        this.phobia = phobia[rand(phobia)];
+        this.hobby = hobby[rand(hobby)];
+        this.firstFact = firstFact[rand(firstFact)];
+        this.secondFact = secondFact[rand(secondFact)];
+        this.backpack = backpack[rand(backpack)];
+        this.actCard = actCard[rand(actCard)];
     }
 }
